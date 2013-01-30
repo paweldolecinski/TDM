a3f2de8f7cb8adf3b79415c55e64428f8a8f4131
do1ek <pawel@dolecinski.pl>
2013-01-30 15:16:16 +0100

    Names refactored

--- tdm.client.gwt/src/main/java/pl/dolecinski/supdicium/client/presenter/inbox/view/ProblemListUiHandlers.java
+++
@@ -1,7 +0,0 @@
-package pl.dolecinski.supdicium.client.presenter.inbox.view;
-
-import com.gwtplatform.mvp.client.UiHandlers;
-
-public interface ProblemListUiHandlers extends UiHandlers {
-	void refreshProblemList(String filter);
-}
