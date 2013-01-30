4398c6b888aaa5a10bec12b1c9a9906410d62281
do1ek <pawel@dolecinski.pl>
2013-01-30 18:40:58 +0100

    Test stub

---
+++ tdm.master/tdm.server/tdm.server.logic/src/test/java/com/tdm/server/logic/problems/GDMProblemAdministration.java
@@ -0,0 +1,15 @@
+package com.tdm.server.logic.problems;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GDMProblemAdministration {
+
+	@Test
+	public void shouldCreateNewGDMProblem() {
+		
+		fail("Not yet implemented");
+	}
+
+}
