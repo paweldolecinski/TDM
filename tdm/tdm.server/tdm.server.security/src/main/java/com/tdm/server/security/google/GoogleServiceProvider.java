2cfaabd4a24eb973b5df35fa4fec8963c1beae4f
do1ek <pawel@dolecinski.pl>
2013-04-16 20:10:58 +0200

    Small config

--- tdm/tdm.server/tdm.server.security/src/main/java/com/tdm/server/security/google/GoogleServiceProvider.java
+++ tdm/tdm.server/tdm.server.security/src/main/java/com/tdm/server/security/google/GoogleServiceProvider.java
@@ -4,15 +4,15 @@ import org.springframework.social.google.api.Google;
 import org.springframework.social.oauth2.AbstractOAuth2ServiceProvider;
 
 public class GoogleServiceProvider extends
-	AbstractOAuth2ServiceProvider<Google> {
+		AbstractOAuth2ServiceProvider<Google> {
 
-    public GoogleServiceProvider(String clientId, String clientSecret) {
-	super(new GoogleOAuth2Template(clientId, clientSecret));
-    }
+	public GoogleServiceProvider(String clientId, String clientSecret) {
+		super(new GoogleOAuth2Template(clientId, clientSecret));
+	}
 
-    @Override
-    public Google getApi(String accessToken) {
-	return new GoogleTemplate(accessToken);
-    }
+	@Override
+	public Google getApi(String accessToken) {
+		return new GoogleTemplate(accessToken);
+	}
 
 }
